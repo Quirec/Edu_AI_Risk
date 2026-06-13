8ec5bbcffcd6030bace8a1d59043896b1eab3409
Quirec <A01745050@tec.mx>
2026-06-13 03:41:29 -0600

    Initial cookiecutter project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='una solución simplificada con enfoque de Inteligencia Artificial que busca apoyar a profesores en la identificación temprana de estudiantes con riesgo de reprobar una materia.',
+    author='Equipo 5',
+    license='MIT',
+)
